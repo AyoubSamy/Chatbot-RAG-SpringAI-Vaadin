e2f20e01a447eb96555ecfcb50e78798ad9b9e2c
7ayoub15 <SAMY82696@GMAIL.COM>
2025-05-11 20:05:19 +0100

    Configuration of RAG and storing the embedding in a file .json

--- src/main/java/net/ayoub/projet_professionnel_chatbot_essaie/Services/ChatService.java
+++ src/main/java/net/ayoub/projet_professionnel_chatbot_essaie/Services/ChatService.java
@@ -3,7 +3,15 @@ package net.ayoub.projet_professionnel_chatbot_essaie.Services;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.ai.document.Document;
+import java.util.List;
+import java.util.Map;
 
 // dans java le symbole @ au debut d'un nom signifie la declaration d'une annotation
 // Une annotation est une métadonnée (une information supplémentaire) que tu ajoutes
@@ -14,14 +22,28 @@ import org.springframework.stereotype.Service;
 @AnonymousAllowed //authoriser l'acces annonyme pour ne pas forcement passer par l''authentification
 
 public class ChatService {
-    private ChatClient chatClient ; // pour se connecter a une partie serveur pour envoyer des question et recuperer des reponses
+    private ChatClient chatClient ;// pour se connecter a une partie serveur pour envoyer des question et recuperer des reponses
+    private VectorStore vectorStore;
+    @Value("classpath:/prompts/prompt-template.st")
+    private Resource PromptResource;
 
-    public ChatService(ChatClient.Builder builder){
+    public ChatService(ChatClient.Builder builder,VectorStore vectorStore){
         this.chatClient= builder.build();
-
+        this.vectorStore=vectorStore;
     }
+
     public String RagChat(String question){
-        return chatClient.prompt() // prompter le client c-a-d le LLM
+        List<Document> documents = vectorStore.similaritySearch(question); // chercher le contexte sous forme d'une liste de documents en fesant
+        // une recherche par similarity (le texte qui est plus proche en terme de sense a la question prompter)
+
+        List<String> contexte = documents.stream().map(Document::getText).toList();
+
+        PromptTemplate promptTemplate = new PromptTemplate(PromptResource);
+
+        Prompt prompt = promptTemplate.create(Map.of("context",contexte,"question",question)); // injection des donner dans le fichier prompt-template.st remplire la variable
+        //contexte en donnant le contexte recuperer dans data externe et la question a repondre
+
+        return chatClient.prompt(prompt) // prompter le client c-a-d le LLM
                 .user(question)
                 .call()
                 .content();
--- src/main/java/net/ayoub/projet_professionnel_chatbot_essaie/config/RagDataLoader.java
+++ src/main/java/net/ayoub/projet_professionnel_chatbot_essaie/config/RagDataLoader.java
@@ -1,16 +1,52 @@
 package net.ayoub.projet_professionnel_chatbot_essaie.config;
+
 import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.stereotype.Component;
-import org.springframework.core.io.Resource;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.document.Document;
 
 
-//@Component
-//public class RagDataLoader {
-//    @Value("classpath:/rag_documents/elements communicon1")
-//    private Resource pdfResource;
-//    @Value("store-data-v1.json")
-//    private String storeFile;
-//
-//
-//}
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+
+
+@Component
+public class RagDataLoader {
+    @Value("classpath:/rag_documents/test_rag.pdf")
+    private Resource pdfResource;
+
+    @Value("store-data-v1.json")
+    private String storeFile;
+    @Bean
+    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel){
+         SimpleVectorStore vectorStore = SimpleVectorStore.builder(embeddingModel).build();
+
+         String fileStore = Path.of("src","main","resources","store")
+                 .toAbsolutePath()+"/"+storeFile;
+
+         File file = new File(fileStore);
+         if(!file.exists()){
+            PagePdfDocumentReader pagePdfDocumentReader=
+                    new PagePdfDocumentReader(pdfResource);
+             List<Document> documents = pagePdfDocumentReader.get();
+
+             TextSplitter textSplitter = new TokenTextSplitter();
+             List<Document> chunks = textSplitter.split(documents);
+             vectorStore.accept(chunks);
+             vectorStore.save(file);
+         } else {
+             // si ona deja nos deocument dans notre vectore store il ne faut que les charger
+         vectorStore.load(file);
+         }
+            return vectorStore;
+         }
+
+}
